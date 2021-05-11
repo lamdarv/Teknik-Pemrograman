59aeef8088624b226e7de6f015c1658281921cb2
lamdarv <78491230+lamdarv@users.noreply.github.com>
2021-05-11 22:54:50 +0700

    Studi Kasus ATM

---
+++ Studi Kasus ATM/ATMMain.java
@@ -0,0 +1,11 @@
+public class ATMMain {
+   // main method creates and runs the ATM
+   public static void main(String[] args) {
+      ATM theATM = new ATM();    
+      theATM.run();
+   }
+} 
+
+
+
+ 
---
+++ Studi Kasus ATM/Transaction.java
@@ -0,0 +1,36 @@
+// Transaction.java
+// Abstract superclass Transaction represents an ATM transaction
+
+public abstract class Transaction {
+   private int accountNumber; // indicates account involved
+   private Screen screen; // ATM's screen
+   private BankDatabase bankDatabase; // account info database
+
+   // Transaction constructor invoked by subclasses using super()
+   public Transaction(int userAccountNumber, Screen atmScreen, 
+      BankDatabase atmBankDatabase) {
+
+      accountNumber = userAccountNumber;
+      screen = atmScreen;
+      bankDatabase = atmBankDatabase;
+   }
+
+   // return account number 
+   public int getAccountNumber() {
+      return accountNumber; 
+   } 
+
+   // return reference to screen
+   public Screen getScreen() {
+      return screen;
+   } 
+
+   // return reference to bank database
+   public BankDatabase getBankDatabase() {
+      return bankDatabase;
+   } 
+
+   // perform the transaction (overridden by each subclass)
+   abstract public void execute();
+} 
+ 
